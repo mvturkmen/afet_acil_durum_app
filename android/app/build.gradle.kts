6059a0ec0584eedbc989791720b70f8ba75c30e8
mvturkmen <93825482+mvturkmen@users.noreply.github.com>
2025-05-20 16:54:10 +0300

    Revert "kullanıcı bilgi ekleme ve düzenleme sayfası eklendi"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,6 @@ android {
     namespace = "com.example.afet_acil_durum_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
